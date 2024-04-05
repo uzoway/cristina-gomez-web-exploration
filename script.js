7f1f6770fb6fb87f4bcb7b74704a1411bc20c571
Uzochukwu Okafor <uzochukwuokafor01@gmail.com>
2024-04-05 23:07:40 +0100

    feat: Change the css approach to a gsap one for the intro animation

--- script.js
+++ script.js
@@ -2,40 +2,58 @@ document.querySelector("body").addEventListener("click", () => {
     document.querySelector(".overlay").classList.toggle("active");
 })
 
+
 gsap.registerPlugin(CustomEase, Flip);
 
-CustomEase.create("ease-in-out-quart", "0.77,0,0.175,1");
+CustomEase.create("ease-out-quad", "0.25,0.46,0.45,0.94");
+
 
 function initAnimation() {
     const state = Flip.getState(".images__item:nth-last-child(8)");
-    const offsetState = Flip.getState(".images__item.offset");
+    const offsetState = Flip.getState(".images__item.offset__image");
 
     gsap.to(".images__item", {
-        "--y-percent": "100%",
+        "--y-percent": "105%",
+        ease: "ease-out-quad",
+        duration: 1.12,
         stagger: {
-            each: 0.1, 
+            each: 0.065,
             from: "edges"
         },
-        delay: 2,
+        delay: 0.7,
         onComplete: () => {
+            gsap.set(".images__item:nth-last-child(8)", { 
+                scale: 2.9, 
+                zIndex: 3, 
+                x: 40, 
+                "grid-column": "7/8", 
+                "grid-row": "1" 
+            })
 
-            document.querySelector(".images__item:nth-last-child(8)").classList.add("flip");
-            const newOffset = document.querySelectorAll(".images__item.offset");
+            const offsetImages = gsap.utils.toArray(".images__item.offset__image");
 
-            newOffset.forEach(item => { item.classList.add("new-flip") })
-            
-            // document.querySelectorAll(".images__item.offset").classList.add("new-flip");
+            gsap.set(offsetImages, { "grid-column": "7/8", "grid-row": "1", x: 20 })
 
             Flip.from(state, {
-                // duration: 1,
-                // ease: "back",
-                // absolute: true,
+                duration: 2.6,
+                ease: "power4.in",
                 scale: true
             })
+
             Flip.from(offsetState, {
-                scale: true,
-                delay: 1
-            });
+                delay: 0.6,
+                ease: "power4.in",
+                duration: 1.12,
+                stagger: {
+                    from: "center",
+                    each: 0.06
+                },
+                onComplete: () => {
+                    gsap.set(offsetImages, { autoAlpha: 0 })
+                }
+            })
+            .to("#translate-y-text", { y: 0, ease: "power4.out", duration: 1.5 }, "+=0.5")
+            .to("#translate-footer-text", { y: 25, ease: "power4.out", duration: 1.5 }, "<")
         }
     })
 }
